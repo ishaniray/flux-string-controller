dafa925758d21bdb122297e0853ab6aaa8604857
Ishani Ray <ishani_ray@yahoo.co.in>
2020-04-10 16:12:21 +0530

    Adding logger.

--- src/main/java/org/cerner/flux/controller/FluxRestController.java
+++ src/main/java/org/cerner/flux/controller/FluxRestController.java
@@ -1,5 +1,7 @@
 package org.cerner.flux.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +11,10 @@ import reactor.core.publisher.Mono;
 
 @RestController
 public class FluxRestController {
+	private static final Logger LOGGER = LoggerFactory.getLogger(FluxRestController.class);
 
 	@PostMapping("/")
 	public Mono<Void> receiveFlux(@RequestBody Flux<String> textStream) {
-		return textStream.doOnNext(System.out::println).then();
+		return textStream.doOnNext(LOGGER::info).then();
 	}
 }
